9587342ce8de2acc1c4cf2e187480748462db9a5
Jan Lukes <jl@cordiq.com>
2016-12-21 17:31:19 +0100

    Data processing not done yet.

--- DataProcessing/DataProcessing.py
+++ DataProcessing/DataProcessing.py
@@ -1,6 +1,19 @@
-file = open("twitter_output.txt")
+file = open("/Users/JanLukes/Documents/GitHub/TwitterDataLake/TwitterDataLake/twitter_output.txt")
 
 columns = []
 for line in file:
-    columns.append(line.split(' ', 7))
+    pom = line.split(' ', 6)
+    for i, each in enumerate(pom):
+        pom[i] = each.strip()
+
+    pom2 = pom[-1].split('Wind', 1)
+    for i, each in enumerate(pom2):
+        pom2[i] = each.strip()
+
+    pom3 = pom2[-1].split('..', 1)
+    for i, each in enumerate(pom3):
+        pom3[i] = each.strip()
+
+    columns.append(pom[:-1] + pom2[:-1] + pom3[:-1])    
+    
 print(columns[0])
