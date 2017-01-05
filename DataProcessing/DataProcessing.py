059a223ec178c399d3da374c56ea30251d2001fc
Jan Lukes <jl@cordiq.com>
2017-01-05 10:50:02 +0100

    Completed the parser.

--- DataProcessing/DataProcessing.py
+++ DataProcessing/DataProcessing.py
@@ -2,18 +2,30 @@ file = open("/Users/JanLukes/Documents/GitHub/TwitterDataLake/TwitterDataLake/tw
 
 columns = []
 for line in file:
-    pom = line.split(' ', 6)
+    pom = line.split(' ')
     for i, each in enumerate(pom):
         pom[i] = each.strip()
-
-    pom2 = pom[-1].split('Wind', 1)
-    for i, each in enumerate(pom2):
-        pom2[i] = each.strip()
-
-    pom3 = pom2[-1].split('..', 1)
-    for i, each in enumerate(pom3):
-        pom3[i] = each.strip()
-
-    columns.append(pom[:-1] + pom2[:-1] + pom3[:-1])    
     
-print(columns[0])
+    pom2 = pom[1:4]
+    for i, each in enumerate(pom):
+        if each == 'Error':
+            pom2 = []
+            break
+        if each == 'Temperature':
+            pom2.append(pom[i+1])
+            j = 2
+            pom3 = ''
+            while not (pom[i+j] == 'Wind' or pom[i+j] == 'Humidity'):
+                pom3 += pom[i+j] + ' '
+                j += 1
+            if pom3:
+                pom2.append(pom3.strip())
+        if each == 'Wind':
+            pom2.append(pom[i+1])
+            pom2.append(pom[i+2])
+        if each == 'Humidity':
+            pom2.append(pom[i+1])
+    columns.append(pom2)
+
+columns = [x for x in columns if x]
+print(columns[0:30])
